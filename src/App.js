5f2341a6717d8aa90b591ad512006d811e497b13
Yura <yurij.kulik.06@mail.ru>
2024-12-12 00:08:29 +0300

    Изменения в App.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Ты сильнейший потому что сатору годжо? Или ты сатору годжо потому что сильнейший?</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
